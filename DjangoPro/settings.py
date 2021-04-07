f80e7a6a8d1b96e707d825af515c4f1108a26551
Narayansingh85 <singhnarayan424@gmail.com>
2021-04-07 10:02:46 +0530

    Adding pip Requirements and try except block in settings

--- DjangoPro/settings.py
+++ DjangoPro/settings.py
@@ -152,3 +152,8 @@ EMAIL_PORT = 587
 EMAIL_HOST_USER='traversy.narayan@gmail.com'
 EMAIL_HOST_PASSWORD='12345'
 EMAIL_USE_TLS=True
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
